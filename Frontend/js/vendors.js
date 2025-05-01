e07b734a43a307f665b39efc5300a94b08e783c7
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-05-01 07:02:44 +0000

    Update vendors.js

--- Frontend/js/vendors.js
+++ Frontend/js/vendors.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8085';
+const API_BASE_URL = 'http://13.53.216.29:8085';
 const PLACEHOLDER_IMAGE = 'https://via.placeholder.com/200';
 
 document.addEventListener('DOMContentLoaded', () => {
